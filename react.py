c7206efd5925cbdc8feedcb86765f4731c52cadb
swacky <student@AWESOME.local>
2014-09-22 15:55:52 +1000

    update react

--- react.py
+++ react.py
@@ -1,93 +1,115 @@
 from browser import document,timer,alert,html
 import time
-from random import random
-start=time.time()
-last=start
-state='intro'
-count=0
-interval=None
-gaps=[]
-
+#from random import random as random1
 doc=document
+
+def random():
+  return .5
 def echo(ev):
-    alert(document["zone"].value)
-def encode(gaps):
-    return ['{:.3f}'.format(gap) for gap in gaps]
-def doOrange():
-    global interval
-    timer.clear_timeout(interval)
+  alert(doc["zone"].value)
+def encode(values):
+  return [str(v) for v in values]
+def showHide(elt,mode=None):
+  inv=doc[elt].style.visibility
+  print('v',elt,inv)
+  if mode == None:
+    mode = inv == 'hidden'
+  doc[elt].style.visibility = '' if mode else 'hidden'
+
+class StopLights:
+  def __init__(self,subjects,start):
+    self.subjects=subjects
+    self.interval = None
+    self.green = False
+    self.last = start
+    self.state = 'intro'
+    self.count = None
+    self.gaps=[]
+    
+  def doOrange(self):
+    timer.clear_timeout(self.interval)
     showHide('red',0)
     showHide('orange',1)
     showHide('green',0)
-    interval=timer.set_interval(doGreen,2000 + random()*5000)
-def doGreen():
-    global green,last,interval
-    timer.clear_timeout(interval)
+    self.interval=timer.set_interval(self.doGreen,2000 + random()*5000)
+    
+  def doGreen(self):
+    timer.clear_timeout(self.interval)
     showHide('orange',0)
     showHide('green',1)
-    green= True
-    last= time.time()
-
-def clicker(ev):
-    global state,count,timer,interval,green,gaps
-    if state== 'intro':
-        state = 'test'
-        green =False
-        doc['zone'].value = 'test'
-        count = 3
-        interval=timer.set_interval(doOrange,2000)
-    elif state in ('test','main'):
-        if green:
-            green = False
-            gap=time.time()-last
-            gaps.append(gap)
-            doc["zone"].value += '\n{}'.format(gap)
-            count -= 1
-            if count ==0:
-                if state== 'test':
-                    green =False
-                    gaps=[]
-                    doc['zone'].value = state = 'main'
-                    count = 5
-                    interval=timer.set_interval(doOrange,2000)
-                else:
-                    state = 'done'
-                    doc['result'].value = '{},{},{},{}'.format('nm','gn','yr'
-                    ,','.join(encode(gaps)))
-                    doc['zone'].value = 'please email results to '
-            else: #do another test
-                interval=timer.set_interval(doOrange,2000)
-        else:  # pused to early so restart timer
-            timer.clear_timeout(interval)
-            interval=timer.set_interval(doGreen,2000 + random()*5000)
+    self.green= True
+    self.last= time.time()
+    
+  def intro(self):
+    self.state = 'test'
+    green =False
+    doc['zone'].value = 'test'
+    self.count = 3
+    self.interval=timer.set_interval(self.doOrange,2000)
+    
+  def response(self):
+    gap=time.time()-self.last
+    self.gaps.append(gap)
+    doc["zone"].value += '\n{}'.format(gap)
+    self.count -= 1
+    if self.count ==0:
+      if self.state== 'test':
+        self.gaps=[]
+        doc['zone'].value = self.state = 'main'
+        self.count = 2
+      else:
+        self.state = 'done'
+        self.subjects[-1].setgaps(self.gaps)
+        doc['result'].value = '\n'.join(
+          [subject.result() for subject in self.subjects])
+        doc['zone'].value = 'please email results to '
+    if self.count > 0: #do another test
+      showHide('green',0)
+      showHide('red',1)
+      self.interval=timer.set_interval(self.doOrange,2000)    
+  def falsePress(self):
+    timer.clear_timeout(self.interval)
+    self.interval=timer.set_interval(self.doGreen,2000 + random()*5000)
+       
+  def clicker(self,ev):
+    if self.state== 'intro':
+      self.intro()
+    elif self.state in ('test','main'):
+      if self.green:
+        self.green = False
+        self.response()
+      else:  # pused to early so restart timer
+        self.falsePress()
     else: # state 'done'
-          pass
-def calc():
- global last
- now=time.time()
- gap=now - last
- last=now
- doc["zone"].value= '{}'.format(gap)
-def showHide(elt,mode=None):
- inv=doc[elt].style.visibility
- print('v',elt,inv)
- if mode == None:
-   mode = inv == 'hidden'
- doc[elt].style.visibility = '' if mode else 'hidden'
+      pass
+      
 
-def ready(ev):
- #flip('mybutton')
- print('--{}--'.format(doc["name"].value))
- if doc['name'].value.strip() == '':
-   doc['error'].value = 'name please'
-   return
- for elt in ('ready','lights','notetab'):
-   showHide(elt)
- doc['zone'].value ='There will be 3 test runs\n press click to start'
+
+class Subject:  
+  def ready(self,ev):
+    print('--{}--'.format(doc["name"].value))
+    if doc['name'].value.strip() == '':
+      doc['error'].value = 'name please'
+      return
+    for elt in ('ready','lights','notetab'):
+      showHide(elt)
+    doc['zone'].value ='There will be 3 test runs\n press click to start'
+    self.name = doc['name'].value
+    self.gender = doc['gender'].value
+    self.year= doc['year'].value
+  def setgaps(self,gaps):
+    self.gaps=[str(gap) for gap in gaps]
+  def result(self):
+    return '{},{},{},{}'.format(self.name,self.gender,self.year
+          , ','.join(encode(self.gaps)))
+    
 showHide('green')
 showHide('orange')
 showHide('lights')
-doc['mybutton'].bind('click',clicker)
-doc['readybutton'].bind('click',ready)
+subjects=[Subject()]
+start=time.time()
+stops=StopLights(subjects,start)
+doc['mybutton'].bind('click',stops.clicker)
+doc['readybutton'].bind('click',subjects[-1].ready)
 
 print("hi")
